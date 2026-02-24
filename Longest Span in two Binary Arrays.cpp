ee16b5da554e1c57d5fbce696c99b714cfb3cc74
Pratyush Kumar <139534961+PratyushK9@users.noreply.github.com>
2026-02-24 23:33:55 +0530

    Implement function to find longest equal sum span

---
+++ Longest Span in two Binary Arrays.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    int equalSumSpan(vector<int> &a1, vector<int> &a2) {
+        
+        int n = a1.size();
+        
+        unordered_map<int, int> mp; 
+        int prefixSum = 0;
+        int maxLen = 0;
+        
+        for (int i = 0; i < n; i++) {
+            
+            int diff = a1[i] - a2[i];
+            prefixSum += diff;
+            
+            // If prefix sum becomes 0
+            if (prefixSum == 0) {
+                maxLen = i + 1;
+            }
+            
+            // If prefix sum seen before
+            if (mp.find(prefixSum) != mp.end()) {
+                maxLen = max(maxLen, i - mp[prefixSum]);
+            } 
+            else {
+                mp[prefixSum] = i;  
+        }
+        
+        return maxLen;
+    }
+};
