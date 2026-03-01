698751dd9bb47f10aa23fee79b5b35d01e45b07b
Pratyush Kumar <139534961+PratyushK9@users.noreply.github.com>
2026-03-01 23:27:42 +0530

    Implement function to move zeros to the end

---
+++ Move_Zeros_to_the_end.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        int count = 0;  // position to place next non-zero
+        
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr[i] != 0) {
+                swap(arr[i], arr[count]);
+                count++;
+            }
+        }
+    }
+};
